0bafd4adb3b8a8254795ca518951fb34b304cb75
Rorac Johnson <roracjohnson@gmail.com>
2024-11-01 18:30:05 -0700

    Add files via upload
    added /config/connection.js

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetworkDB';
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = mongoose.connection;
